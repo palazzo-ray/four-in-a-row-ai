ade310f7f0ea8799ffd71b5e2e3a3cf447c23288
palazzo-ray <ray.wkwong@outlook.com>
2019-01-03 16:58:45 +0800

    reshape for fitting and predict

--- four_ai/agents/four_dqn_agent.py
+++ four_ai/agents/four_dqn_agent.py
@@ -12,15 +12,16 @@ import os.path
 from ..utils.logger import logger
 from ..config.config import Config
 
+
 class BaseModel():
-    def __init__(self,model_name, model_save_path):
+    def __init__(self, model_name, model_save_path):
         self.model_save_path = model_save_path
         self.model_name = model_name
 
-    def save_model( self ):
+    def save_model(self):
         file_name = self.model_save_path + '/' + self.model_name
 
-        model = self.model 
+        model = self.model
 
         model_json_file = file_name + '_model.json'
         model_weight_file = file_name + '_weight.h5'
@@ -32,8 +33,8 @@ class BaseModel():
         model.save_weights(model_weight_file)
         logger.info("Saved model to disk : " + str(file_name))
 
-    def load_model( self, loaded_model ):
-        self.model = loaded_model 
+    def load_model(self, loaded_model):
+        self.model = loaded_model
         self._compile_model()
 
     def load_model_from_file(self):
@@ -56,7 +57,7 @@ class BaseModel():
         # load weights into new model
         loaded_model.load_weights(model_weight_file)
 
-        self.load_model( loaded_model )
+        self.load_model(loaded_model)
         logger.info("Loaded model from disk : " + str(file_name))
 
         return True
@@ -65,14 +66,13 @@ class BaseModel():
         act_values = self.model.predict(state)
 
         return act_values
+
     def fit(self, batch_x, batch_y, epochs=1, verbose=0):
         fit_result = self.model.fit(batch_x, batch_y, epochs=1, verbose=0)
 
         loss = fit_result.history["loss"][0]
         accuracy = fit_result.history["acc"][0]
         return loss, accuracy
-
-
 
 
 ####################
@@ -82,15 +82,14 @@ class BaseModel():
 class DQNModel(BaseModel):
     model_name = 'NN_128x16'
 
-    def __init__(self, action_size , board_size, model_save_path='.'):
+    def __init__(self, action_size, board_size, model_save_path='.'):
         super(DQNModel, self).__init__(DQNModel.model_name, model_save_path)
 
         self.learning_rate = 0.001
-        self.input_dim = np.prod( board_size )
+        self.input_dim = np.prod(board_size)
 
         self.action_size = action_size
         self.model = self._build_model()
-
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
@@ -104,12 +103,16 @@ class DQNModel(BaseModel):
         return model
 
     def _compile_model(self):
-        self.model.compile(loss='mse',
-                      optimizer=Adam(lr=self.learning_rate),
-                      metrics=["accuracy"])
+        self.model.compile(
+            loss='mse',
+            optimizer=Adam(lr=self.learning_rate),
+            metrics=["accuracy"])
 
-    def state_conversion(self,state):
-        state = state.reshape( [1, self.input_dim])
+        logger.info('compile model')
+        self.model.summary(print_fn=logger.info)
+
+    def state_conversion(self, state):
+        state = state.reshape([1, self.input_dim])
 
         return state
 
@@ -121,36 +124,32 @@ class DQNModel(BaseModel):
 class DQN_CNN_Model(BaseModel):
     model_name = 'CNN_32x64x128'
 
-    def __init__(self, action_size , board_size, model_save_path='.'):
-        super(DQN_CNN_Model, self).__init__(DQN_CNN_Model.model_name, model_save_path)
+    def __init__(self, action_size, board_size, model_save_path='.'):
+        super(DQN_CNN_Model, self).__init__(DQN_CNN_Model.model_name,
+                                            model_save_path)
 
         self.learning_rate = 0.001
-        self.input_shape = ( board_size[0] , board_size[1] , 1 )
+        self.input_shape = (board_size[0], board_size[1], 1)
+        self.input_shape_batch = (1, board_size[0], board_size[1], 1)
 
         self.learning_rate = 0.00025
-        self.learning_rho=0.95
-        self.learning_epsilon=0.01
-
-        print('dkkdkdkdkddkdkkd')
-        print(self.input_shape)
+        self.learning_rho = 0.95
+        self.learning_epsilon = 0.01
 
         self.action_size = action_size
         self.model = self._build_model()
 
-
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Conv2D(32,
-                              (4,4),
-                              activation="relu",
-                              input_shape=self.input_shape,
-                              data_format="channels_last"))
-        model.add(Conv2D(64,
-                              (3,3),
-                              activation="relu",
-                              data_format="channels_last"
-                              ))
+        model.add(
+            Conv2D(
+                32, (4, 4),
+                activation="relu",
+                input_shape=self.input_shape,
+                data_format="channels_last"))
+        model.add(
+            Conv2D(64, (3, 3), activation="relu", data_format="channels_last"))
         model.add(Flatten())
         model.add(Dense(128, activation="relu"))
         model.add(Dense(self.action_size, activation='linear'))
@@ -159,29 +158,28 @@ class DQN_CNN_Model(BaseModel):
         self._compile_model()
         return model
 
-
-    def state_conversion(self,state):
-        print('state_conver')
-        print(state)
-        print(state.shape)
-        state = state.reshape(self.input_shape)
-        print('new shape')
-        print(state.shape)
+    def state_conversion(self, state):
+        state = state.reshape(self.input_shape_batch)
         return state
 
     def _compile_model(self):
-        self.model.compile(loss="mean_squared_error",
-                            optimizer=RMSprop(lr=self.learning_rate,
-                                             rho=self.learning_rho,
-                                             epsilon=self.learning_epsilon),
-                            metrics=["accuracy"])
+        self.model.compile(
+            loss="mean_squared_error",
+            optimizer=RMSprop(
+                lr=self.learning_rate,
+                rho=self.learning_rho,
+                epsilon=self.learning_epsilon),
+            metrics=["accuracy"])
+
+        logger.info('compile model')
+        self.model.summary(print_fn=logger.info)
+
+        self.model.compile(
+            loss='mse',
+            optimizer=Adam(lr=self.learning_rate),
+            metrics=["accuracy"])
 
 
-        self.model.compile(loss='mse',
-                      optimizer=Adam(lr=self.learning_rate),
-                      metrics=["accuracy"])
-
- 
 # Deep Q-learning Agent
 class DQNAgent():
     '''
@@ -190,48 +188,44 @@ class DQNAgent():
     if now when the agent is asked to hold '+1' button, we flip all the state variables from -1 to 1 and 1 to -1
     '''
 
-    def __init__(self, 
-                    who, model_name, 
-                    load_model =False,
-                    save_learnt_to_file=False,
-                    board_size = ( 6, 7) , action_size = 7 
-                    ):
+    def __init__(self,
+                 who,
+                 model_name,
+                 load_model=False,
+                 save_learnt_to_file=False,
+                 board_size=(6, 7),
+                 action_size=7):
 
         self.board_size = board_size
         self.action_size = action_size
         self.save_learnt_to_file = save_learnt_to_file
 
         self.memory = collections.deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.gamma = 0.95  # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.30
         self.epsilon_decay = 0.9995
 
-
         self.fitting_cb = None
 
-
-        if who == 'player' :
-            self.button_color_invert = 1 # to multiple the state by this varible. meaning no change
+        if who == 'player':
+            self.button_color_invert = 1  # to multiple the state by this varible. meaning no change
         else:
             # who == 'npc'
-            self.button_color_invert = -1 # to multiple the state by this varible. meaning -1 to 1 , 1 to -1 
+            self.button_color_invert = -1  # to multiple the state by this varible. meaning -1 to 1 , 1 to -1
 
-
-        model_save_path  = Config.Folder.TRAINED_FOLDER 
+        model_save_path = Config.Folder.TRAINED_FOLDER
         if not os.path.exists(model_save_path):
             os.makedirs(model_save_path)
 
         models = {
-            DQNModel.model_name  : DQNModel,
-            DQN_CNN_Model.model_name : DQN_CNN_Model
+            DQNModel.model_name: DQNModel,
+            DQN_CNN_Model.model_name: DQN_CNN_Model
         }
 
         if model_name is None:
-            model_name = DQN_CNN_Model.model_name 
+            model_name = DQN_CNN_Model.model_name
 
-        print('jjjjjjjjjjjjjjjjjjjjj')
-        print('model ' + str(model_name))
         self.model_creator = lambda : models[model_name](action_size , self.board_size, model_save_path=model_save_path)
         #self.model = models[model_name](action_size , self.board_size, model_save_path=model_save_path)
         self.model = self.model_creator()
@@ -243,36 +237,33 @@ class DQNAgent():
             if not model_exist:
                 logger.info('New model file will be created while learn')
 
-
     def add_fitting_callback(self, cb):
-        self.fitting_cb = cb 
+        self.fitting_cb = cb
 
     def _remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def _flip_state(self,state):
-        board, _ , _ = state 
+    def _flip_state(self, state):
+        board, _, _ = state
 
         if self.button_color_invert == -1:
-            board = board * self.button_color_invert 
+            board = board * self.button_color_invert
 
-        return board 
+        return board
 
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             action = random.randrange(self.action_size)
             return action
-        
+
         #state = ( self.board.copy() , act_row, act_col )
         state = self._flip_state(state)
         state = self.model.state_conversion(state)
         act_values = self.model.predict(state)
-        
+
         action = np.argmax(act_values[0])  # returns action
 
         return action
-
-
 
     def learn(self, state, action, reward, next_state, done):
         #logger.info('state')
@@ -285,21 +276,22 @@ class DQNAgent():
         next_state = self.model.state_conversion(next_state)
 
         self._remember(state, action, reward, next_state, done)
-        self._replay(done, batch_size = 16)
+        self._replay(done, batch_size=16)
 
-
-    def _get_target_state_action_value(self,next_state):
+    def _get_target_state_action_value(self, next_state):
         predict_next_state_action_values = self.model.predict(next_state)
-        return predict_next_state_action_values 
+        return predict_next_state_action_values
 
-    def _get_training_x_y( self, state, action, reward, next_state, done ):
+    def _get_training_x_y(self, state, action, reward, next_state, done):
         target = reward
-        
+
         if not done:
-            predict_next_state_action_values = self._get_target_state_action_value(next_state)
-            
-            max_next_state_action_value = np.amax( predict_next_state_action_values[0] )
-            
+            predict_next_state_action_values = self._get_target_state_action_value(
+                next_state)
+
+            max_next_state_action_value = np.amax(
+                predict_next_state_action_values[0])
+
             target = reward + self.gamma * max_next_state_action_value
 
         target_f = self.model.predict(state)
@@ -308,13 +300,13 @@ class DQNAgent():
 
         max_q = np.max(target_f[0])
 
-        return state , target_f , max_q
+        return state, target_f, max_q
 
     def _replay(self, done, batch_size):
-        memory_size = len( self.memory )
+        memory_size = len(self.memory)
         # if done | (memory_size >= batch_size ):
-        if (memory_size >= batch_size ):
-            batch_size = min( memory_size , batch_size )
+        if (memory_size >= batch_size):
+            batch_size = min(memory_size, batch_size)
 
             minibatch = random.sample(self.memory, batch_size)
 
@@ -322,62 +314,65 @@ class DQNAgent():
             train_y = []
             max_q = []
             for state, action, reward, next_state, done in minibatch:
-                x, y , q = self._get_training_x_y( state, action, reward, next_state, done )
+                x, y, q = self._get_training_x_y(state, action, reward,
+                                                 next_state, done)
 
-                train_x.append( x )
-                train_y.append( y )
-                max_q.append( q )
+                train_x.append(x)
+                train_y.append(y)
+                max_q.append(q)
 
-            batch_x = np.concatenate( train_x )
-            batch_y = np.concatenate( train_y )
+            batch_x = np.concatenate(train_x)
+            batch_y = np.concatenate(train_y)
 
             #self.model.fit(x, y, epochs=1, verbose=0)
-            loss, accuracy = self.model.fit(batch_x, batch_y, epochs=1, verbose=0)
+            loss, accuracy = self.model.fit(
+                batch_x, batch_y, epochs=1, verbose=0)
             mean_q = np.mean(max_q)
 
             if self.fitting_cb is not None:
-                self.fitting_cb(loss,accuracy, mean_q)
+                self.fitting_cb(loss, accuracy, mean_q)
 
             if self.epsilon > self.epsilon_min:
                 self.epsilon *= self.epsilon_decay
 
-
     def save_model(self):
-        if self.save_learnt_to_file :
+        if self.save_learnt_to_file:
             self.model.save_model()
         else:
             logger.info('This agent is not for saving new learning to file')
 
 
-
-
 class DDQNAgent(DQNAgent):
-# Double Deep Q-learning Agent
+    # Double Deep Q-learning Agent
     '''
     The agent assumes he is holding '-1'  button,  the env is holding '+1'  when training
 
     if now when the agent is asked to hold '+1' button, we flip all the state variables from -1 to 1 and 1 to -1
     '''
-    def __init__(self, 
-                    who, model_name, 
-                    load_model =False,
-                    save_learnt_to_file=False,
-                    board_size = ( 6, 7) , action_size = 7 
-                    ):
-        super(DDQNAgent, self).__init__( who=who, model_name=model_name, load_model =load_model,
-                                        save_learnt_to_file=save_learnt_to_file,
-                                        board_size = board_size, action_size=action_size)
 
+    def __init__(self,
+                 who,
+                 model_name,
+                 load_model=False,
+                 save_learnt_to_file=False,
+                 board_size=(6, 7),
+                 action_size=7):
+        super(DDQNAgent, self).__init__(
+            who=who,
+            model_name=model_name,
+            load_model=load_model,
+            save_learnt_to_file=save_learnt_to_file,
+            board_size=board_size,
+            action_size=action_size)
 
         ### create target network model
         self.target_model = self.model_creator()
         self.update_target_network()
 
-    def _get_target_state_action_value(self,next_state):
-        predict_next_state_action_values = self.target_model.predict(next_state)
-        return predict_next_state_action_values 
+    def _get_target_state_action_value(self, next_state):
+        predict_next_state_action_values = self.target_model.predict(
+            next_state)
+        return predict_next_state_action_values
 
     def update_target_network(self):
         self.target_model.model.set_weights(self.model.model.get_weights())
-
-    
--- four_ai/config/config.py
+++ four_ai/config/config.py
@@ -1,56 +1,65 @@
 from ..utils.logger import logger
 
+
 class BaseConfig():
     @staticmethod
     def print_info(x):
-        logger.info('Trainer.TARGET_NETWORK_UPDATE_FREQUENCY : ' + str(x.Trainer.TARGET_NETWORK_UPDATE_FREQUENCY ) )
-        logger.info('Trainer.MODEL_PERSISTENCE_UPDATE_FREQUENCY : ' + str(x.Trainer.MODEL_PERSISTENCE_UPDATE_FREQUENCY ) )
-        logger.info('Trainer.LOG_FILE_SAVING_FREQUENCY : ' + str(x.Trainer.LOG_FILE_SAVING_FREQUENCY ) )
-        logger.info('Trainer.NUM_OF_ROUND : ' + str(x.Trainer.NUM_OF_ROUND ) )
-        logger.info('Trainer.NUM_OF_ITERATION : ' + str(x.Trainer.NUM_OF_ITERATION ) )
+        logger.info('Trainer.TARGET_NETWORK_UPDATE_FREQUENCY : ' +
+                    str(x.Trainer.TARGET_NETWORK_UPDATE_FREQUENCY))
+        logger.info('Trainer.MODEL_PERSISTENCE_UPDATE_FREQUENCY : ' +
+                    str(x.Trainer.MODEL_PERSISTENCE_UPDATE_FREQUENCY))
+        logger.info('Trainer.LOG_FILE_SAVING_FREQUENCY : ' +
+                    str(x.Trainer.LOG_FILE_SAVING_FREQUENCY))
+        logger.info('Trainer.NUM_OF_ROUND : ' + str(x.Trainer.NUM_OF_ROUND))
+        logger.info('Trainer.NUM_OF_ITERATION : ' +
+                    str(x.Trainer.NUM_OF_ITERATION))
 
-        logger.info('Stats.TRAINING_UPDATE_FREQUENCY  : ' + str(x.Stats.TRAINING_UPDATE_FREQUENCY ) )
-        logger.info('Stats.RUN_UPDATE_FREQUENCY  : ' + str(x.Stats.RUN_UPDATE_FREQUENCY ) )
-        logger.info('Stats.MAX_LOSS : ' + str(x.Stats.MAX_LOSS) )
+        logger.info('Stats.TRAINING_UPDATE_FREQUENCY  : ' +
+                    str(x.Stats.TRAINING_UPDATE_FREQUENCY))
+        logger.info('Stats.RUN_UPDATE_FREQUENCY  : ' +
+                    str(x.Stats.RUN_UPDATE_FREQUENCY))
+        logger.info('Stats.MAX_LOSS : ' + str(x.Stats.MAX_LOSS))
 
-        logger.info('Folder.TRAINED_FOLDER: ' + str(x.Folder.TRAINED_FOLDER) )
+        logger.info('Folder.TRAINED_FOLDER: ' + str(x.Folder.TRAINED_FOLDER))
+
 
 class Cloud_Config(BaseConfig):
-
     class Trainer():
-        TARGET_NETWORK_UPDATE_FREQUENCY =  20000 
-        MODEL_PERSISTENCE_UPDATE_FREQUENCY = 5000 
+        TARGET_NETWORK_UPDATE_FREQUENCY = 8000
+        MODEL_PERSISTENCE_UPDATE_FREQUENCY = 5000
         LOG_FILE_SAVING_FREQUENCY = 1000
 
-        NUM_OF_ROUND = 100
-        NUM_OF_ITERATION = 80000
+        NUM_OF_ROUND = 10000
+        NUM_OF_ITERATION = 25000
 
     class Stats():
-        TRAINING_UPDATE_FREQUENCY = 5000 
-        RUN_UPDATE_FREQUENCY = 500 
+        TRAINING_UPDATE_FREQUENCY = 500
+        RUN_UPDATE_FREQUENCY = 500
         MAX_LOSS = 1
 
     class Folder():
         TRAINED_FOLDER = './training_model/'
 
+
 class Test_Config(BaseConfig):
-
     class Trainer():
-        TARGET_NETWORK_UPDATE_FREQUENCY = 1 
-        MODEL_PERSISTENCE_UPDATE_FREQUENCY =1 
-        LOG_FILE_SAVING_FREQUENCY = 1 
+        TARGET_NETWORK_UPDATE_FREQUENCY = 1
+        MODEL_PERSISTENCE_UPDATE_FREQUENCY = 1
+        LOG_FILE_SAVING_FREQUENCY = 1
 
-        NUM_OF_ROUND = 1 
-        NUM_OF_ITERATION = 20 
+        NUM_OF_ROUND = 1
+        NUM_OF_ITERATION = 20
 
     class Stats():
         #TRAINING_UPDATE_FREQUENCY = 1000
         #RUN_UPDATE_FREQUENCY = 10
-        TRAINING_UPDATE_FREQUENCY = 1 
-        RUN_UPDATE_FREQUENCY = 1 
+        TRAINING_UPDATE_FREQUENCY = 1
+        RUN_UPDATE_FREQUENCY = 1
         MAX_LOSS = 1
 
     class Folder():
         TRAINED_FOLDER = './training_model/'
 
-Config = Test_Config
+
+#Config = Test_Config
+Config = Cloud_Config
