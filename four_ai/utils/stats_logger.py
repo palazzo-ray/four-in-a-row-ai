b17c8df3e955d301536bb9c1a7b32943abd910e1
palazzo-ray <ray.wkwong@outlook.com>
2019-01-04 14:25:47 +0800

    more stats

--- four_ai/utils/stats_logger.py
+++ four_ai/utils/stats_logger.py
@@ -12,25 +12,30 @@ from ..config.config import Config
 matplotlib.use("Agg")
 
 
-
 class StatsLogger:
-
     def __init__(self, header, directory_path):
         directory_path = directory_path
         if not os.path.exists(directory_path):
             os.makedirs(directory_path)
 
-        self.score = Stat("run", "score", Config.Stats.RUN_UPDATE_FREQUENCY, directory_path, header)
-        self.step = Stat("run", "step", Config.Stats.RUN_UPDATE_FREQUENCY, directory_path, header)
-        self.loss = Stat("update", "loss", Config.Stats.TRAINING_UPDATE_FREQUENCY, directory_path, header)
-        self.accuracy = Stat("update", "accuracy", Config.Stats.TRAINING_UPDATE_FREQUENCY, directory_path, header)
-        self.q = Stat("update", "q", Config.Stats.TRAINING_UPDATE_FREQUENCY, directory_path, header)
+        self.score = Stat("run", "score", Config.Stats.RUN_UPDATE_FREQUENCY,
+                          directory_path, header)
+        self.step = Stat("run", "step", Config.Stats.RUN_UPDATE_FREQUENCY,
+                         directory_path, header)
+        self.loss = Stat("update", "loss",
+                         Config.Stats.TRAINING_UPDATE_FREQUENCY,
+                         directory_path, header)
+        self.accuracy = Stat("update", "accuracy",
+                             Config.Stats.TRAINING_UPDATE_FREQUENCY,
+                             directory_path, header)
+        self.q = Stat("update", "q", Config.Stats.TRAINING_UPDATE_FREQUENCY,
+                      directory_path, header)
 
     def add_run(self, run):
 
         #if run % RUN_UPDATE_FREQUENCY == 0:
         #    logger.info('{{"metric": "run", "value": {}}}'.format(run))
-        return 
+        return
 
     def add_score(self, run, score):
         self.score.add_entry(run, score)
@@ -42,21 +47,23 @@ class StatsLogger:
         self.accuracy.add_entry(fit, accuracy)
 
     def add_loss(self, fit, loss):
-        loss = min(Config.Stats.MAX_LOSS, loss)  # Loss clipping for very big values that are likely to happen in the early stages of learning
+        loss = min(
+            Config.Stats.MAX_LOSS, loss
+        )  # Loss clipping for very big values that are likely to happen in the early stages of learning
         self.loss.add_entry(fit, loss)
 
     def add_q(self, fit, q):
         self.q.add_entry(fit, q)
 
-    def log_iteration(self, episode, reward , t):
+    def log_iteration(self, episode, reward, t):
         self.add_run(episode)
         self.add_score(episode, reward)
         self.add_step(episode, t)
 
-    def log_fitting(self, fit_time, loss,accuracy, q):
+    def log_fitting(self, fit_time, loss, accuracy, q):
         self.add_accuracy(fit_time, accuracy)
         self.add_loss(fit_time, loss)
-        self.add_q(fit_time,q)
+        self.add_q(fit_time, q)
 
     def save_csv(self):
         self.score.save_file()
@@ -67,8 +74,8 @@ class StatsLogger:
 
 
 class Stat:
-
-    def __init__(self, x_label, y_label, update_frequency, directory_path, header):
+    def __init__(self, x_label, y_label, update_frequency, directory_path,
+                 header):
         self.x_label = x_label
         self.y_label = y_label
         self.update_frequency = update_frequency
@@ -84,29 +91,38 @@ class Stat:
         self._append_save_csv(self.csv_path, self.xy_values)
         self.xy_values = []
 
-
     def save_png(self):
-        self._save_png(input_path=self.csv_path,
-                        output_path=self.png_path,
-                        small_batch_length=self.update_frequency,
-                        big_batch_length=self.update_frequency*10,
-                        x_label=self.x_label,
-                        y_label=self.y_label)
+        self._save_png(
+            input_path=self.csv_path,
+            output_path=self.png_path,
+            small_batch_length=self.update_frequency,
+            big_batch_length=self.update_frequency * 10,
+            x_label=self.x_label,
+            y_label=self.y_label)
 
     def add_entry(self, x_value, y_value):
         self.temp_y_values.append(y_value)
         if len(self.temp_y_values) % self.update_frequency == 0:
-            mean_value = np.mean(self.temp_y_values)
-            min_value = min(self.temp_y_values)
-            max_value = max(self.temp_y_values)
+
+            y_values = np.array( self.temp_y_values)
+
+            mean_value = np.mean(y_values)
+            min_value = np.min(y_values)
+            max_value = np.max(y_values)
+            min_count = np.count_nonzero(y_values == min_value)
+            max_count = np.count_nonzero(y_values == max_value)
 
             timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-            self.xy_values.append( [ x_value, mean_value , min_value, max_value , timestamp ] )
+            self.xy_values.append([
+                timestamp, x_value, mean_value, min_value, max_value,
+                min_count, max_count
+            ])
 
             self.temp_y_values = []
 
-    def _save_png(self, input_path, output_path, small_batch_length, big_batch_length, x_label, y_label):
+    def _save_png(self, input_path, output_path, small_batch_length,
+                  big_batch_length, x_label, y_label):
         x = []
         y = []
         with open(input_path, "r") as scores:
@@ -120,7 +136,6 @@ class Stat:
 
         plt.subplots()
         plt.plot(x, y, label="last " + str(small_batch_length) + " average")
-
         '''
         batch_averages_y = []
         batch_averages_x = []
@@ -155,10 +170,15 @@ class Stat:
 
     def _append_save_csv(self, path, xy_values):
         if not os.path.exists(path):
-            with open(path, "w", newline=''):
-                pass
+            scores_file = open(path, "w", newline='')
+            with scores_file:
+                writer = csv.writer(scores_file)
+                writer.writerow([
+                    'Timestamp', self.x_label, 'mean', 'min', 'max',
+                    'min_count', 'max_count'
+                ])
 
-        scores_file = open(path, "a" , newline='')
+        scores_file = open(path, "a", newline='')
         with scores_file:
             writer = csv.writer(scores_file)
 
