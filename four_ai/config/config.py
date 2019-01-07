8324fa9a6a480dc8306fd8232aa523c94dc6bb7e
palazzo-ray <ray.wkwong@outlook.com>
2019-01-07 14:41:52 +0800

    config for longer term stability

--- four_ai/config/config.py
+++ four_ai/config/config.py
@@ -7,29 +7,28 @@ class BaseConfig():
         LEARNING_RHO = 0.95
         LEARNING_EPSILON = 0.01
 
-        NORMAL_MEMORY_SIZE =    200000
-        WINNING_MEMORY_SIZE =   50000
-        LOSSING_MEMORY_SIZE =   50000
+        NORMAL_MEMORY_SIZE = 200000
+        WINNING_MEMORY_SIZE = 50000
+        LOSSING_MEMORY_SIZE = 50000
         IMPORTANT_MEMORY_SIZE = 50000
 
-        START_TRAINING_SIZE = 5000
+        START_TRAINING_SIZE = 80
         BATCH_SIZE = 32
-        BATCH_NORMAL = 13 #  41%
+        BATCH_NORMAL = 13  #  41%
         BATCH_WINNING = 5  # 16%
-        BATCH_LOSSING = 3 # 9%  - lossing 
-        BATCH_IMPORTANT = 11  # 34% have more than important num of button on the board 
+        BATCH_LOSSING = 3  # 9%  - lossing
+        BATCH_IMPORTANT = 11  # 34% have more than important num of button on the board
 
-        NUM_BUTTON_PLAYED_AS_IMPORTANT = 18 # 18 button played
-
+        NUM_BUTTON_PLAYED_AS_IMPORTANT = 15  # 15 button played
 
     class Explorer():
         GAMMA = 0.95  # DISCOUNT RATE
         EPSILON = 0.8  # EXPLORATION RATE
         EPSILON_MID = 0.4
-        EPSILON_DECAY_TO_MID = 0.999996
+        EPSILON_DECAY_TO_MID = 0.999997
 
         EPSILON_MIN = 0.12
-        EPSILON_DECAY_TO_MIN = 0.999995
+        EPSILON_DECAY_TO_MIN = 0.999996
 
     @staticmethod
     def print_info(x):
@@ -41,41 +40,40 @@ class BaseConfig():
 
         logger.info('Stats.TRAINING_UPDATE_FREQUENCY  : ' + str(x.Stats.TRAINING_UPDATE_FREQUENCY))
         logger.info('Stats.RUN_UPDATE_FREQUENCY  : ' + str(x.Stats.RUN_UPDATE_FREQUENCY))
-        logger.info('Stats.MAX_LOSS : ' + str(x.Stats.MAX_LOSS)) 
+        logger.info('Stats.MAX_LOSS : ' + str(x.Stats.MAX_LOSS))
 
-        logger.info('Optimizer.LEARNING_RATE : ' + str(x.Optimizer.LEARNING_RATE ) )
-        logger.info('Optimizer.LEARNING_RHO : ' + str(x.Optimizer.LEARNING_RHO ) )
-        logger.info('Optimizer.LEARNING_EPSILON : ' + str(x.Optimizer.LEARNING_EPSILON) )
-        logger.info('Optimizer.NORMAL_MEMORY_SIZE: ' + str(x.Optimizer.NORMAL_MEMORY_SIZE) )
-        logger.info('Optimizer.WINNING_MEMORY_SIZE: ' + str(x.Optimizer.WINNING_MEMORY_SIZE) )
-        logger.info('Optimizer.LOSSING_MEMORY_SIZE: ' + str(x.Optimizer.LOSSING_MEMORY_SIZE) )
-        logger.info('Optimizer.IMPORTANT_MEMORY_SIZE: ' + str(x.Optimizer.IMPORTANT_MEMORY_SIZE) )
+        logger.info('Optimizer.LEARNING_RATE : ' + str(x.Optimizer.LEARNING_RATE))
+        logger.info('Optimizer.LEARNING_RHO : ' + str(x.Optimizer.LEARNING_RHO))
+        logger.info('Optimizer.LEARNING_EPSILON : ' + str(x.Optimizer.LEARNING_EPSILON))
+        logger.info('Optimizer.NORMAL_MEMORY_SIZE: ' + str(x.Optimizer.NORMAL_MEMORY_SIZE))
+        logger.info('Optimizer.WINNING_MEMORY_SIZE: ' + str(x.Optimizer.WINNING_MEMORY_SIZE))
+        logger.info('Optimizer.LOSSING_MEMORY_SIZE: ' + str(x.Optimizer.LOSSING_MEMORY_SIZE))
+        logger.info('Optimizer.IMPORTANT_MEMORY_SIZE: ' + str(x.Optimizer.IMPORTANT_MEMORY_SIZE))
 
-        logger.info('Optimizer.BATCH_SIZE : ' + str(x.Optimizer.BATCH_SIZE) )
-        logger.info('Optimizer.BATCH_NORMAL : ' + str(x.Optimizer.BATCH_NORMAL ) )
-        logger.info('Optimizer.BATCH_WINNING : ' + str(x.Optimizer.BATCH_WINNING ) )
-        logger.info('Optimizer.BATCH_LOSSING : ' + str(x.Optimizer.BATCH_LOSSING ) )
-        logger.info('Optimizer.BATCH_IMPORTANT: ' + str(x.Optimizer.BATCH_IMPORTANT) )
+        logger.info('Optimizer.BATCH_SIZE : ' + str(x.Optimizer.BATCH_SIZE))
+        logger.info('Optimizer.BATCH_NORMAL : ' + str(x.Optimizer.BATCH_NORMAL))
+        logger.info('Optimizer.BATCH_WINNING : ' + str(x.Optimizer.BATCH_WINNING))
+        logger.info('Optimizer.BATCH_LOSSING : ' + str(x.Optimizer.BATCH_LOSSING))
+        logger.info('Optimizer.BATCH_IMPORTANT: ' + str(x.Optimizer.BATCH_IMPORTANT))
 
-        logger.info('Explorer.GAMMA : ' + str( x.Explorer.GAMMA ) )
-        logger.info('Explorer.EPSILON : ' + str( x.Explorer.EPSILON ) )
-        logger.info('Explorer.EPSILON_MID : ' + str( x.Explorer.EPSILON_MID ) )
-        logger.info('Explorer.EPSILON_DECAY_TO_MID : ' + str( x.Explorer.EPSILON_DECAY_TO_MID ) )
-        logger.info('Explorer.EPSILON_MIN : ' + str( x.Explorer.EPSILON_MIN ) )
-        logger.info('Explorer.EPSILON_DECAY_TO_MIN : ' + str( x.Explorer.EPSILON_DECAY_TO_MIN ) )
+        logger.info('Explorer.GAMMA : ' + str(x.Explorer.GAMMA))
+        logger.info('Explorer.EPSILON : ' + str(x.Explorer.EPSILON))
+        logger.info('Explorer.EPSILON_MID : ' + str(x.Explorer.EPSILON_MID))
+        logger.info('Explorer.EPSILON_DECAY_TO_MID : ' + str(x.Explorer.EPSILON_DECAY_TO_MID))
+        logger.info('Explorer.EPSILON_MIN : ' + str(x.Explorer.EPSILON_MIN))
+        logger.info('Explorer.EPSILON_DECAY_TO_MIN : ' + str(x.Explorer.EPSILON_DECAY_TO_MIN))
 
         logger.info('Folder.TRAINED_FOLDER: ' + str(x.Folder.TRAINED_FOLDER))
 
 
 class Cloud_Config(BaseConfig):
     class Trainer():
-        TARGET_NETWORK_UPDATE_FREQUENCY = 4000
+        TARGET_NETWORK_UPDATE_FREQUENCY = 20000
         MODEL_PERSISTENCE_UPDATE_FREQUENCY = 5000
         LOG_FILE_SAVING_FREQUENCY = 1000
 
         NUM_OF_ROUND = 10000
-        NUM_OF_ITERATION = 8000
-
+        NUM_OF_ITERATION = 100000
 
     class Stats():
         TRAINING_UPDATE_FREQUENCY = 100
